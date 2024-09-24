2421535b98272b4aee9c2255801ecc9dac1ec470
Mouna <mounaamouna252@gmail.com>
2024-09-24 18:09:22 +0200

    version 2

---
+++ Desktop/Challenge/Untitled1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    float Km,Yards;
+    printf("la distance en kilomètres est: \n");
+    scanf("%f",&Km);
+    Yards = Km + 1093.61;
+    printf("la transforme en yards est :%2f",Yards);
+    return 0;
+}
